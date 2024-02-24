f046e5b9ee71f85e72490cc7556aafbea8873385
Maryanne-Emilia <pensymaryanne@gmail.com>
2024-02-24 09:33:49 +0300

    Initial commit

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name=input("what is your Name: ")
+age=int(input("How old are you: "))
+location=input("Where is your location: ")
+print(f"Hello {name} your {age} years old and live in {location} ")
